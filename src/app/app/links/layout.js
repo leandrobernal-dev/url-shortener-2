7ddd122dd8beda3b4e1dfaf5cba15a080bd69a42
Leandro Bernal <bernalleandro.dev@gmail.com>
2023-08-31 14:15:27 +0800

    removed link details page background, moved links details page styling up to layout, added padding

--- src/app/app/links/layout.js
+++ src/app/app/links/layout.js
@@ -3,10 +3,13 @@
 export default function LinksLayout({ children }) {
     return (
         <div className="flex gap-1 h-full">
-            <div className="dark:bg-zinc-800 rounded-sm h-full w-full sm:w-72">
+            <div className="dark:bg-zinc-800 p-1 rounded-sm h-full w-full sm:w-72">
                 links here
             </div>
-            {children}
+            {/* {children} */}
+            <div className="p-1 rounded-sm hidden sm:flex items-center justify-center flex-1">
+                {children}
+            </div>
         </div>
     );
 }
--- src/app/app/links/page.js
+++ src/app/app/links/page.js
@@ -1,7 +1,3 @@
 export default function Links() {
-    return (
-        <div className="dark:bg-zinc-800 rounded-sm hidden sm:flex items-center justify-center flex-1">
-            Select link to view details
-        </div>
-    );
+    return <div className="">Select link to view details</div>;
 }
