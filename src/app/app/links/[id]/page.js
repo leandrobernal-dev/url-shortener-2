e7bb2d67841e07fc88928b9e210eb5d2e1642e88
Leandro Bernal <bernalleandro.dev@gmail.com>
2023-09-01 15:49:58 +0800

    installed mapchart, created MapChart component

--- src/app/app/links/[id]/page.js
+++ src/app/app/links/[id]/page.js
@@ -1,12 +1,14 @@
 "use client";
 
+import MapChart from "@/components/MapChart";
 import { Close, Info } from "@mui/icons-material";
 import { useRouter } from "next/navigation";
 
 export default function LinksDetails({ params }) {
     const router = useRouter();
+    const urlId = params.id;
     return (
-        <div className="fixed top-0 right-0 left-0 bottom-0 dark:bg-black sm:static pt-[60px] sm:pt-0 p-1">
+        <div className="fixed top-1 right-1 left-1 small-scrollbar bottom-1 w-full overflow-y-scroll h-full dark:bg-black sm:static pt-[60px] sm:pt-0">
             <nav className="fixed flex items-center top-1 h-14 left-1 right-1 dark:bg-zinc-800 sm:hidden">
                 <div className="w-full flex justify-between p-2">
                     <span className="flex items-center gap-1">
@@ -17,7 +19,13 @@ export default function LinksDetails({ params }) {
                     </button>
                 </div>
             </nav>
-            <h1 className="text-white">{params.id}</h1>
+            <MapChart
+                data={[
+                    { id: "PH", count: 21 },
+                    { id: "JP", count: 451 },
+                ]}
+                id={"noen"}
+            />
         </div>
     );
 }
---
+++ src/components/MapChart.js
@@ -0,0 +1,87 @@
+import { useEffect } from "react";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+import am4geodata_worldLow from "@amcharts/amcharts4-geodata/worldLow";
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4maps from "@amcharts/amcharts4/maps";
+
+function MapChart({ data, id }) {
+    useEffect(() => {
+        // Apply the animated theme
+        am4core.useTheme(am4themes_animated);
+
+        // Create the map chart instance
+        const chart = am4core.create(id, am4maps.MapChart);
+        chart.geodata = am4geodata_worldLow;
+
+        // Disable map interactions only when the cursor is over the map
+        chart.chartContainer.events.on("wheel", (event) => {
+            event.event.stopPropagation();
+        });
+        // chart.interactionsEnabled = false;
+        chart.seriesContainer.draggable = false;
+        chart.seriesContainer.hoverable = true;
+        chart.maxZoomLevel = 1;
+
+        // Set the projection
+        chart.projection = new am4maps.projections.Miller();
+
+        // Create the polygon series for displaying regions
+        const polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+        polygonSeries.useGeodata = true;
+
+        // Configure the polygon series
+        polygonSeries.calculateVisualCenter = true;
+        polygonSeries.exclude = ["AQ"]; // Exclude Antarctica from the map
+
+        // Add data to the polygon series
+        polygonSeries.data = data;
+
+        // Configure the polygon template
+        const polygonTemplate = polygonSeries.mapPolygons.template;
+        polygonTemplate.tooltipText = "{name}: {count}";
+        polygonTemplate.nonScalingStroke = true;
+
+        // Disable hover on regions with 0 values
+        polygonSeries.mapPolygons.template.events.on("over", (event) => {
+            const { dataItem } = event.target;
+            if (!dataItem.dataContext.count) {
+                // event.preventDefault();
+                event.target.isHover = false;
+            }
+        });
+
+        // Customize the region color based on count
+        polygonSeries.mapPolygons.template.adapter.add(
+            "fill",
+            (fill, target) => {
+                const { dataItem } = target;
+                if (dataItem.dataContext) {
+                    if (dataItem.dataContext.count) {
+                        return am4core.color("#005F74"); //#414141 Darker color for regions with views
+                    } else {
+                        return am4core.color("#20A6B9"); //#CFCFCF Light gray color for regions with no views
+                    }
+                }
+                return fill;
+            }
+        );
+
+        return () => {
+            // Cleanup
+            chart.dispose();
+        };
+    }, [data]);
+
+    return (
+        <div
+            id={id}
+            className="rounded-sm dark:bg-zinc-950"
+            style={{
+                width: "100%",
+                height: "500px",
+            }}
+        />
+    );
+}
+
+export default MapChart;
