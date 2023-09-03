950d84aadffd687136c9315e3ea9ea7ea6b1bcd4
Leandro Bernal <bernalleandro.dev@gmail.com>
2023-09-04 06:51:00 +0800

    moved up link details page nav z-index

--- src/app/app/links/[id]/page.js
+++ src/app/app/links/[id]/page.js
@@ -29,7 +29,7 @@ export default function LinksDetails({ params }) {
     });
     return (
         <div className="fixed top-1 right-1 left-1 small-scrollbar bottom-1 w-full overflow-y-scroll h-full dark:bg-black sm:static pt-[60px] sm:pt-0">
-            <nav className="fixed flex items-center top-1 h-14 left-1 right-1 dark:bg-zinc-800 sm:hidden">
+            <nav className="fixed flex z-50 items-center top-1 h-14 left-1 right-1 dark:bg-zinc-800 sm:hidden">
                 <div className="w-full flex justify-between p-2">
                     <span className="flex items-center gap-1">
                         <Info /> Link Details
