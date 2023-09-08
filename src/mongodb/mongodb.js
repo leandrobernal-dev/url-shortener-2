9a0a9badf922a43ae02ed6248f46f050445997df
Leandro Bernal <bernalleandro.dev@gmail.com>
2023-09-08 17:17:41 +0800

    minor update

--- src/mongodb/mongodb.js
+++ src/mongodb/mongodb.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const dbConnect = async () => {
     console.log("Connecting to Database...");
-    console.log(process.env.MONGODB_URI);
     if (mongoose.connections[0].readyState) {
         console.log("Connected to Database");
         return true;
