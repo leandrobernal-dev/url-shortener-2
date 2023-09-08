101a9dbacd61903f3d86b3c9ca80eecda47d3dee
Leandro Bernal <bernalleandro.dev@gmail.com>
2023-09-08 16:55:29 +0800

    created mongodb connect function

---
+++ src/mongodb/mongodb.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const dbConnect = async () => {
+    console.log("Connecting to Database...");
+    console.log(process.env.MONGODB_URI);
+    if (mongoose.connections[0].readyState) {
+        console.log("Connected to Database");
+        return true;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Connected to Database");
+        return true;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export default dbConnect;
